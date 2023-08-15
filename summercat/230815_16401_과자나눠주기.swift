ef5b79776b51ea1de61c17f99668290177a09f4f
summercat <98260324+dev-summer@users.noreply.github.com>
2023-08-15 16:18:21 +0900

    Create 230815_16401_과자나눠주기.swift

---
+++ summercat/230815_16401_과자나눠주기.swift
@@ -0,0 +1,30 @@
+let input = readLine()!.split(separator: " ").compactMap { Int(String($0)) }
+let sticks: [Int] = readLine()!.split(separator: " ").compactMap { Int(String($0)) }
+
+var m: Int = input[0]
+var n: Int = input[1]
+
+// 길이: target
+// target 이상 과자가 m개 이상이어야 한다.
+// 쪼갤 수 있다 -> 15를 7 7 로 나누어서 2명에게 주기 가능
+
+func solve(){
+    var left: Int = 0
+    var right: Int = sticks.max()!
+    
+    while left < right {
+        let mid = (left + right + 1) / 2
+        var count: Int = 0
+        
+        for i in 0..<n {
+            count += sticks[i] / mid
+        }
+        
+        if count >= m { left = mid }
+        else { right = mid - 1 }
+    }
+
+    print(left)
+}
+
+solve()
